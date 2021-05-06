756140d4eb52825fe724402a6a908b4e9c3dc8b3
Jake Moe <JakesAddress@gmail.com>
2021-05-06 18:50:53 +1000

    End of Section 9

--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,6 +1,8 @@
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CommonModule } from '@angular/common';
+import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgModule } from '@angular/core';
+import { TabsModule } from 'ngx-bootstrap/tabs';
 import { ToastrModule } from 'ngx-toastr';
 
 @NgModule({
@@ -8,14 +10,12 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     BsDropdownModule.forRoot(),
     CommonModule,
+    NgxGalleryModule,
+    TabsModule.forRoot(),
     ToastrModule.forRoot({
-      positionClass: 'toast-bottom-right'
+      positionClass: 'toast-bottom-right',
     })
   ],
-  exports: [
-    BsDropdownModule,
-    ToastrModule
-  ]
+  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
 })
-
-export class SharedModule { }
+export class SharedModule {}
--- client/src/app/app.module.ts
+++ client/src/app/app.module.ts
@@ -4,9 +4,10 @@ import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { BrowserModule } from '@angular/platform-browser';
 import { FormsModule } from '@angular/forms';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { HomeComponent } from './home/home.component';
 import { NavComponent } from './nav/nav.component';
 import { NgModule } from '@angular/core';
-import { HomeComponent } from './home/home.component';
+
 import { RegisterComponent } from './register/register.component';
 import { MemberListComponent } from './members/member-list/member-list.component';
 import { MemberDetailComponent } from './members/member-detail/member-detail.component';
@@ -17,33 +18,36 @@ import { TestErrorsComponent } from './errors/test-errors/test-errors.component'
 import { ErrorInterceptor } from './_interceptors/error.interceptor';
 import { NotFoundComponent } from './errors/not-found/not-found.component';
 import { ServerErrorComponent } from './errors/server-error/server-error.component';
+import { MemberCardComponent } from './members/member-card/member-card.component';
+import { JwtInterceptor } from './_interceptors/jwt.interceptor';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HomeComponent,
-    ListsComponent,
-    MemberDetailComponent,
-    MemberListComponent,
-    MessagesComponent,
     NavComponent,
+    HomeComponent,
     RegisterComponent,
+    MemberListComponent,
+    MemberDetailComponent,
+    ListsComponent,
+    MessagesComponent,
     TestErrorsComponent,
     NotFoundComponent,
-    ServerErrorComponent
+    ServerErrorComponent,
+    MemberCardComponent,
   ],
   imports: [
-    AppRoutingModule,
-    BrowserAnimationsModule,
     BrowserModule,
-    FormsModule,
+    AppRoutingModule,
     HttpClientModule,
-    SharedModule
+    BrowserAnimationsModule,
+    FormsModule,
+    SharedModule,
   ],
   providers: [
-    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true }
+    { provide: HTTP_INTERCEPTORS, useClass: ErrorInterceptor, multi: true },
+    { provide: HTTP_INTERCEPTORS, useClass: JwtInterceptor, multi: true },
   ],
-  bootstrap: [AppComponent]
+  bootstrap: [AppComponent],
 })
-
-export class AppModule { }
+export class AppModule {}
