4fd1cc6c9183ba34756c864bcdae9e8b4a5d23aa
Jake Moe <JakesAddress@gmail.com>
2021-05-16 09:54:11 +1000

    Section 11 Lesson 132 Error

--- API/Extensions/ApplicationServiceExtensions.cs
+++ API/Extensions/ApplicationServiceExtensions.cs
@@ -2,7 +2,6 @@ using API.Data;
 using API.Helpers;
 using API.Interfaces;
 using API.Services;
-using AutoMapper;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
@@ -13,10 +12,11 @@ namespace API.Extensions
   {
     public static IServiceCollection AddApplicationServices(this IServiceCollection services, IConfiguration config)
     {
+      services.Configure<CloudinarySettings>(config.GetSection("CloudinarySettings"));
       services.AddScoped<ITokenService, TokenService>();
+      services.AddScoped<IPhotoService, PhotoService>();
       services.AddScoped<IUserRepository, UserRepository>();
       services.AddAutoMapper(typeof(AutoMapperProfiles).Assembly);
-
       services.AddDbContext<DataContext>(options =>
       {
         options.UseSqlite(config.GetConnectionString("DefaultConnection"));
--- client/src/app/_modules/shared.module.ts
+++ client/src/app/_modules/shared.module.ts
@@ -1,5 +1,6 @@
 import { BsDropdownModule } from 'ngx-bootstrap/dropdown';
 import { CommonModule } from '@angular/common';
+import { FileUploadModule } from 'ng2-file-upload';
 import { NgxGalleryModule } from '@kolkov/ngx-gallery';
 import { NgModule } from '@angular/core';
 import { TabsModule } from 'ngx-bootstrap/tabs';
@@ -10,12 +11,19 @@ import { ToastrModule } from 'ngx-toastr';
   imports: [
     BsDropdownModule.forRoot(),
     CommonModule,
+    FileUploadModule,
     NgxGalleryModule,
     TabsModule.forRoot(),
     ToastrModule.forRoot({
       positionClass: 'toast-bottom-right',
     })
   ],
-  exports: [BsDropdownModule, ToastrModule, TabsModule, NgxGalleryModule],
+  exports: [
+    BsDropdownModule,
+    FileUploadModule,
+    NgxGalleryModule,
+    TabsModule,
+    ToastrModule
+  ]
 })
 export class SharedModule {}
